091bd984fd15735ca337001e5f25128aae72184c
Jason Sutter <jasonsutter@Rocko.wavecable.com>
2018-02-14 23:18:50 -0800

    Implement settings button

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
+    protect_from_forgery with: :exception
+
+    before_filter :configure_permitted_parameters, if: :devise_controller?
+
+    protected
+
+    def configure_permitted_parameters
+        devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :username, :email, :password, :bio])
+        devise_parameter_sanitizer.permit(:account_update, keys: [:name, :username, :email, :password, :current_password, :bio])
+    end
 end
