f53fe2098ed2d4fe97ad1ef28a4f25bd41c3dd31
Jason Sutter <jasonsutter@Rocko.wavecable.com>
2018-02-23 17:21:11 -0800

    Create Post show page, linked post to it

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,4 +2,11 @@ class PostsController < ApplicationController
   def index
     @posts = Post.all
   end
+
+  def show
+    @likes = Like.all
+    @photos = Photo.all
+    @posts = Post.all
+    @comments = Comment.all
+  end
 end
