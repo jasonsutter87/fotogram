23ab4226ad6defd8ee07cf8f79888e20465f42fa
Jason Sutter <jasonsutter@Rocko.wavecable.com>
2018-02-23 16:47:32 -0800

    User can like a post

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,5 +1,14 @@
 class LikesController < ApplicationController
+  skip_before_action :verify_authenticity_token
+
   def index
     @likes = Like.all
+  end
+
+  def create
+    Like.create(
+      post_id: params['like']['post_id'],
+      user_id: params['like']['user_id'])
+    redirect_to "/feed"
   end
 end
