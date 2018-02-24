b8b7c291cd1d85207d53e11cf82dd6552da32609
Jason Sutter <jasonsutter@Rocko.wavecable.com>
2018-02-23 16:45:37 -0800

    User can add comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,15 @@
 class CommentsController < ApplicationController
+  skip_before_action :verify_authenticity_token
+
   def index
     @comments = Comment.all
+  end
+
+  def create
+    Comment.create(
+      post_id: params['comment']['post_id'],
+      user_id: params['comment']['user_id'],
+      comment: params['comment']['comment'])
+    redirect_to "/feed"
   end
 end
