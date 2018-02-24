4c93c1dcc993e58da30e124962175c5c39f5d043
Jason Sutter <jasonsutter@Rocko.wavecable.com>
2018-02-23 16:46:12 -0800

    User can follow and unfollow users

--- app/controllers/connections_controller.rb
+++ app/controllers/connections_controller.rb
@@ -1,14 +1,25 @@
 class ConnectionsController < ApplicationController
-  protect_from_forgery with: :exception
+  skip_before_action :verify_authenticity_token
 
   def index
     @connections = Connection.all
   end
 
   def create
-    @user = User.find(params[:id])
-    Connection.create(user_id: current_user.id, following_id: @user.id)
-    redirect_to "/users/#{params[:id]}"
+    Connection.create(
+      user_id: params['connection']['user_id'],
+      following_id: params['connection']['following_id'])
+    redirect_to "/users/#{params['connection']['following_id']}"
   end
+
+  def destroy
+    connection = Connection.find_by(
+      user_id: current_user.id,
+      following_id: params[:id])
+    Connection.destroy(connection.id)
+    user = User.find(params[:id])
+
+    redirect_to "/feed"
+ end
 
 end
