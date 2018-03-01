f412e0bda05a460bdd3b68a1f99dc1829ce20bc3
Jason Sutter <jasonsutter@Rocko.wavecable.com>
2018-03-01 15:57:09 -0800

    Update seed with new data

--- db/seeds.rb
+++ db/seeds.rb
@@ -26,13 +26,18 @@ end
 
 #Create 50 Posts
 50.times do
-  Post.create(user_id: rand(1..20), filter_id: rand(1..10), filter_opacity: rand(0.0..1.0).round(2), photo_id: rand(1..25), description: Faker::Lorem.sentence, location: Faker::Address.city)
+  Post.create(user_id: rand(1..20), filter_id: rand(1..10), filter_opacity: rand(0.0..1.0).round(2), photo_id: rand(25..50), description: Faker::Lorem.sentence, location: Faker::Address.city)
 end
 
 
-#Create 25 Photos
+#Create 25 Photos for users
 25.times do
   Photo.create(url: 'https://robohash.org/' + Faker::Lorem.word + '?set=set3')
+end
+
+#Create 25 Photos for posts
+55.times do
+  Photo.create(url: 'https://picsum.photos/600/600')
 end
 #Create 10 Filters
 10.times do
@@ -40,11 +45,17 @@ end
 end
 
 Photo.create(url: "https://avatars2.githubusercontent.com/u/12101475?s=400&u=ef6c23f12f4a03a1a9d0992625399f9a1532dc92&v=4")
-Photo.create(url: "https://www.organicfacts.net/wp-content/uploads/2013/05/Banana3.jpg")
-User.create(name: 'Jason Sutter', username: 'Evilhoodlem' , bio: 'Tacos everyday!',  photo_id: 26, email: 'jasonsutter87@gmail.com', password: '123456' )
-Post.create(user_id: 51, filter_id: rand(1..10), filter_opacity: rand(0.0..1.0).round(2), photo_id: 27, description: Faker::Lorem.sentence, location: Faker::Address.city)
+User.create(name: 'Jason Sutter', username: 'jasonsutter87' , bio: 'Tacos everyday!',  photo_id: 81, email: 'jason@fotogram.com', password: 'fotogram' )
+Post.create(user_id: 51, filter_id: rand(1..10), filter_opacity: rand(0.0..1.0).round(2), photo_id: 51, description: Faker::Lorem.sentence, location: 'Sacramento, CA')
+Post.create(user_id: 51, filter_id: rand(1..10), filter_opacity: rand(0.0..1.0).round(2), photo_id: 52, description: Faker::Lorem.sentence, location: 'Sacramento, CA')
+Post.create(user_id: 51, filter_id: rand(1..10), filter_opacity: rand(0.0..1.0).round(2), photo_id: 53, description: Faker::Lorem.sentence, location: 'Sacramento, CA')
+Post.create(user_id: 51, filter_id: rand(1..10), filter_opacity: rand(0.0..1.0).round(2), photo_id: 54, description: Faker::Lorem.sentence, location: 'Sacramento, CA')
+Post.create(user_id: 51, filter_id: rand(1..10), filter_opacity: rand(0.0..1.0).round(2), photo_id: 55, description: Faker::Lorem.sentence, location: 'Sacramento, CA')
 Connection.create(user_id: 51, following_id: 1)
 Connection.create(user_id: 51, following_id: 5)
 Connection.create(user_id: 51, following_id: 10)
 Connection.create(user_id: 1, following_id: 51)
 Like.create(user_id: 1, post_id: 51)
+Like.create(user_id: 2, post_id: 51)
+Like.create(user_id: 2, post_id: 52)
+Like.create(user_id: 7, post_id: 53)
